4e0c43a9e6d2cf1ab3e4e4984e0a315f26e816c1
Kiwi_Lin <opda0887@gmail.com>
2024-06-12 21:33:40 +0800

    rearrange folder & create basic pannel

--- mail_gen.java
+++ mail_gen.java
@@ -1,8 +1,11 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import tools.output_meg;
 import tools.read_text;
 import tools.read_title;
 
-public class mail_gen {
+public class mail_gen extends JPanel{
 
   public static void main(String[] args) {
     String title = read_title.main();
@@ -11,8 +14,12 @@ public class mail_gen {
 
     output_meg.main(title, spliter ,text);
     
-    System.out.println(title);
-    System.out.println(spliter);
-    System.out.println(text);
+    System.out.println("Task finished.");
+    
+    // create basic pannel
+    JFrame window = new JFrame();
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    window.setSize(920, 480);
+    window.setVisible(true);
   }
 }
